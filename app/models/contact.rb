f624e6dc6eaa09cb2eb81b6acf333c60fbc47139
Caio <caio.dscamara@gmail.com>
2017-11-12 15:53:24 -0200

    Edição.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,7 @@
 class Contact < ApplicationRecord
   belongs_to :kind, optional: true
   has_many :phones
+  accepts_nested_attributes_for :phones
   def birthdate_br
     I18n.l(self.birthdate) unless self.birthdate.blank?
   end

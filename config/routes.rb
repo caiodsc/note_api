f7a77434a695894af3ae3018a10ea605239131dc
Caio <caio.dscamara@gmail.com>
2017-12-10 14:58:51 -0400

    CRUD AMS com has_many (Phones)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
     resource :kind, only: [:show], path: 'relationships/kind'
 
     resource :phones, only: [:show]
+    resource :phone, only: [:update, :create, :destroy]
     resource :phones, only: [:show], path: 'relationships/phones'
+    resource :phone, only: [:update, :create, :destroy], path: 'relationships/phones'
 
     resource :address, only: [:show, :update, :create, :destroy]
     resource :address, only: [:show, :update, :create, :destroy], path: 'relationships/address'

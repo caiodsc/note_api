ccf7f953dbf3bbcaa511ac80fcddbee3ae8ae920
Caio <caio.dscamara@gmail.com>
2017-12-10 12:12:52 -0400

    CRUD AMS com has_one (Address)

--- app/serializers/address_serializer.rb
+++ app/serializers/address_serializer.rb
@@ -1,3 +1,7 @@
 class AddressSerializer < ActiveModel::Serializer
   attributes :id, :street, :city
+
+  belongs_to :contact do#, optional:
+    link(:related) { contact_address_url(object.contact.id) }
+  end
 end
